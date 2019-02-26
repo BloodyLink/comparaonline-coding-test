0ead0b62b0d7d25dc47c420e3f31cf68035547ce
BloodyLink <david.quezada.m@gmail.com>
2019-02-26 17:20:22 -0300

    base classes added

---
+++ classes/CarInsurance.py
@@ -0,0 +1,8 @@
+class CarInsurance():
+
+    def __init__(self, products):
+        self.products = products
+
+    def updatePrice(self):
+        pass
+
---
+++ classes/Product.py
@@ -0,0 +1,15 @@
+class Product():
+
+    def __init__(self, name, sellIn, price):
+        self.name = name
+        self.sellIn = sellIn
+        self.price = price
+
+    def updatePrice(self):
+        deg = 1
+        if (self.sellIn < 1):
+            deg = 2
+        return max(0, min(50, self.price - deg))
+
+    def updateSellIn(self):
+        return self.sellIn - 1
